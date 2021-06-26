c399a4acfe1acc1a8e4384d123ba09b18baa4eac
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-26 19:00:51 -0300

    Update [...router].js

--- pages/api/v1/[...router].js
+++ pages/api/v1/[...router].js
@@ -1,11 +1,28 @@
-export default (req, res) => {
+export default async (req, res) => {
+
+  async function getData (url) {
+    return await fetch(url)
+    .then(data => data.json())
+  }
 
   const { router } = req.query
+
+  const split = router[0].split("@")
+
+  const user = split[1]
+  const repo = split[0]
+  const schema = router[1]
+  const url = `https://raw.githubusercontent.com/${user}/${repo}/main/${schema}.schema.json`
+
+  const data = await getData(url)
 
   res
     .status(200)
     .json({
-      user: router[0],
-      schema: router[1]
+      user,
+      repo,
+      schema,
+      url,
+      data
     })
 }
