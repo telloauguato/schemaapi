db994a56635aec287016b988a714f2fb1de57237
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-26 17:18:27 -0300

    Update [...router].js

--- pages/api/v1/[...router].js
+++ pages/api/v1/[...router].js
@@ -1,8 +1,5 @@
-import { useRouter } from "next/router";
-
-const { router } = useRouter().query
 
 export default (req, res) => {
   res.statusCode = 200
-  res.json({ name: router })
+  res.json({ query: req.query })
 }
