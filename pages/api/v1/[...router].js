914253eb5e46f6d1ef29643c7ef4cc243e39da64
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-27 04:33:28 -0300

    Update [...router].js

--- pages/api/v1/[...router].js
+++ pages/api/v1/[...router].js
@@ -1,9 +1,13 @@
+
+async function getData(url) {
+  return await fetch(url)
+    .then(data => data.json())
+}
+
 export default async (req, res) => {
 
-  async function getData (url) {
-    return await fetch(url)
-    .then(data => data.json())
-  }
+  let datas = {}
+
 
   const { router } = req.query
 
@@ -16,13 +20,46 @@ export default async (req, res) => {
 
   const data = await getData(url)
 
+  const randomUser = await getData(`https://randomuser.me/api/?results=${data.amount}&nat=${data.lang ? data.lang : 'US'}`)
+  datas.randonUser = randomUser.results
+
+
+  let returns = []
+
+  for (let i = 0; i < data.amount; i++) {
+
+
+
+    for (let j = 0; j < data.content.length; j++) {
+
+      const { key, type, suffix = '', prefix = '', options = [] } = data.content[j];
+      var result
+
+      switch (type) {
+        case "name":
+          result = { [key]: `${prefix}${datas.randonUser[i].name.first} ${datas.randonUser[i].name.last}${suffix}` }
+          break;
+
+        case "username":
+          result = { [key]: `${prefix}${datas.randonUser[i].login.username}${suffix}` }
+          break;
+
+        case "options":
+          result = { [key]: `${prefix}${options[Math.floor(Math.random() * options.length)]}${suffix}` }
+          break;
+
+        default:
+          break;
+      }
+      returns[i] = { ...returns[i], ...result }
+    }
+  }
+
   res
     .status(200)
     .json({
-      user,
-      repo,
-      schema,
-      url,
-      data
+      //data,
+      //datas,
+      results: returns
     })
 }
