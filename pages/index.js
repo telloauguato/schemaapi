7febd735b52e3573fe097139c3d52b8f7f40e8db
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-07-28 17:14:17 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Schema API</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -14,17 +14,6 @@ export default function Home() {
           Schema API
         </h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
