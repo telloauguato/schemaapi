245a13183005d851a72d8f15ba12e2b9eb494824
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-07-28 17:31:05 -0300

    Aparece

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Schema API</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <main className={styles.main}>
