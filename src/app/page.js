ba51a19d07627f65c50e4eb0a92433c17595b881
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2023-09-02 17:02:30 +0000

    ll

--- src/app/page.js
+++ src/app/page.js
@@ -32,35 +32,6 @@ export default function Home() {
       </section>
       <section className="body-font text-gray-600">
         <div className="container mx-auto flex flex-wrap px-5 py-24">
-          <div className="-mx-4 mb-auto mt-auto flex flex-wrap content-start sm:w-2/3 sm:pr-10 lg:w-1/2">
-            <div className="mb-6 w-full px-4 sm:p-4">
-              <h1 className="title-font mb-2 text-xl font-medium text-gray-900">Moon hashtag pop-up try-hard offal truffaut</h1>
-              <div className="leading-relaxed">Pour-over craft beer pug drinking vinegar live-edge gastropub, keytar neutra sustainable fingerstache kickstarter.</div>
-            </div>
-            <div className="w-1/2 p-4 sm:w-1/2 lg:w-1/4">
-              <h2 className="title-font text-3xl font-medium text-gray-900">2.7K</h2>
-              <p className="leading-relaxed">Users</p>
-            </div>
-            <div className="w-1/2 p-4 sm:w-1/2 lg:w-1/4">
-              <h2 className="title-font text-3xl font-medium text-gray-900">1.8K</h2>
-              <p className="leading-relaxed">Subscribes</p>
-            </div>
-            <div className="w-1/2 p-4 sm:w-1/2 lg:w-1/4">
-              <h2 className="title-font text-3xl font-medium text-gray-900">35</h2>
-              <p className="leading-relaxed">Downloads</p>
-            </div>
-            <div className="w-1/2 p-4 sm:w-1/2 lg:w-1/4">
-              <h2 className="title-font text-3xl font-medium text-gray-900">4</h2>
-              <p className="leading-relaxed">Products</p>
-            </div>
-          </div>
-          <div className="mt-6 w-full overflow-hidden rounded-lg sm:mt-0 sm:w-1/3 lg:w-1/2">
-            <Image className="h-full w-full object-cover object-center" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="stats" />
-          </div>
-        </div>
-      </section>
-      <section className="body-font text-gray-600">
-        <div className="container mx-auto flex flex-wrap px-5 py-24">
           <div className="flex w-full flex-wrap">
             <div className="md:w-1/2 md:py-6 md:pr-10 lg:w-2/5">
               <div className="relative flex pb-12">
