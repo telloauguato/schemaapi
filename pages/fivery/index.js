b7e5f21ab509327e4eb28069bc049328cadaf2ab
augusto.marcelo@yahoo.com.br <augusto.marcelo@yahoo.com.br>
2023-09-18 13:38:19 -0300

    Abr

--- pages/fivery/index.js
+++ pages/fivery/index.js
@@ -2,6 +2,17 @@ import { useEffect, useState } from 'react';
 
 function ApiData({ data }) {
   const [apiData, setApiData] = useState(data);
+  const result = [];
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+  for (let i = 0; i < length; i++) {
+    if (arr.length === 0) {
+      break
+    }
+    const i = Math.floor(Math.random() * arr.length);
+    const s = arr.splice(i, 1)[0];
+    result.push(s);
+  }
 
   useEffect(() => {
     // Use esta função para buscar dados da API.
@@ -9,7 +20,7 @@ function ApiData({ data }) {
       try {
         const response = await fetch('https://schemaapi.vercel.app/api/schemaapi@telloauguato/fivery'); // Substitua pela URL da sua API.
         const newData = await response.json();
-        setApiData(newData);
+        setApiData(newData.data);
       } catch (error) {
         console.error('Erro ao buscar dados da API:', error);
       }
@@ -21,7 +32,7 @@ function ApiData({ data }) {
 
   return (
     <div>
-      <h1>Dados da API:</h1>
+      <h1>{result.join(' — ')}</h1>
       <pre>{JSON.stringify(apiData, null, 2)}</pre>
     </div>
   );
