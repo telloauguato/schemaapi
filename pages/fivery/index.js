c83ad6159bb13d38fa553bb450da4044b2ad0097
augusto.marcelo@yahoo.com.br <augusto.marcelo@yahoo.com.br>
2023-09-18 13:39:32 -0300

    Red

--- pages/fivery/index.js
+++ pages/fivery/index.js
@@ -5,7 +5,7 @@ function ApiData({ data }) {
   const result = [];
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < 5; i++) {
     if (arr.length === 0) {
       break
     }
