afc7904ab354a73c2bcb79ea8920e0cf64810cbf
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-06-30 12:14:48 -0300

    🐚

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,21 +1,45 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import pkg from "./package.json";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>≝ SchemaAPI</span>,
+  logo: <span>🐚 SchemaAPI</span>,
   project: {
     link: "https://github.com/telloauguato/schemaapi",
   },
   docsRepositoryBase: "https://github.com/telloauguato/schemaapi",
   footer: {
-    text: `≝ SchemaAPI Docs — ${pkg.version} | Telo`,
+    text: `🐚 SchemaAPI Docs — ${pkg.version}`,
   },
-  faviconGlyph: "≝",
+  faviconGlyph: "🐚",
   i18n: [
     { locale: "en-US", text: "English" },
     { locale: "pt-BR", text: "Português" },
   ],
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – SchemaAPI'
+    }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://schemaapi.vercel.app' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'SchemaAPI'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'SchemaAPI Docs'}
+        />
+      </>
+    )
+  }
 };
 
 export default config;
