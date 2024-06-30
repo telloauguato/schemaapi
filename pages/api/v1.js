24ccdc3d67836d3692de735d782b13e5ef50f226
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-06-30 18:03:52 -0300

    remove api

--- pages/api/v1.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        res.status(200).json({ message: 'Hello from Next.js!' })    
-    } else {
-      // Handle any other HTTP method
-    }
-  }
